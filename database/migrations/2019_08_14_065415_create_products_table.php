b38a21c0db59dc5b22497730c6f1862b3c4af84b
UrAvgCoder <niri1607@gmail.com>
2019-08-14 12:55:41 +0530

    migration for product & reviews

--- database/migrations/2019_08_14_065415_create_products_table.php
+++ database/migrations/2019_08_14_065415_create_products_table.php
@@ -14,7 +14,12 @@ class CreateProductsTable extends Migration
     public function up()
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->increments('id');
+            $table->string('name');
+            $table->text('detail');
+            $table->integer('price');
+            $table->integer('stock');
+            $table->integer('discount');
             $table->timestamps();
         });
     }
--- database/migrations/2019_08_14_065442_create_reviews_table.php
+++ database/migrations/2019_08_14_065442_create_reviews_table.php
@@ -14,7 +14,12 @@ class CreateReviewsTable extends Migration
     public function up()
     {
         Schema::create('reviews', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->increments('id');
+            $table->integer('product_id')->unsigned()->index();
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->string('customer');
+            $table->text('review');
+            $table->integer('star');
             $table->timestamps();
         });
     }
