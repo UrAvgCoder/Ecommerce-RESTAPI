fc40a000065eb935d9365e947332bd228b21b033
UrAvgCoder <niri1607@gmail.com>
2019-08-14 14:23:36 +0530

    Added review link and made some changes in the data returned by API

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -18,8 +18,13 @@ class ProductResource extends JsonResource
             'name' => $this->name,
             'description' => $this->detail,
             'price' => $this->price,
-            'stock' => $this->stock,
-            'discount' => $this->discount
+            'stock' => $this->stock == 0 ? 'Product Out of Stock' : $this->stock,
+            'discount' => $this->discount,
+            'rating' => round($this->reviews->sum('star') / $this->reviews->count('star'), 2),
+            'totalPrice' => round(( 1 - ($this->discount/100)) * $this->price, 2),
+            'href' => [
+                'reviews' => route('reviews.index', $this->id)
+            ]
         ];
     }
 }
