04a51f7bdedf4e540a18f4ebfd39b50554b2d6b5
UrAvgCoder <niri1607@gmail.com>
2019-08-14 13:27:30 +0530

    added relationships between Product & Reviews

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
 }
--- app/Model/Review.php
+++ app/Model/Review.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
