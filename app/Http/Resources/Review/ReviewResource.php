4c5cb2026cd6870d2e9ed14eb9dc0bc551b3728a
UrAvgCoder <niri1607@gmail.com>
2019-08-14 15:02:15 +0530

    Review Resource and Product pagination

---
+++ app/Http/Resources/Review/ReviewResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources\Review;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ReviewResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'customer' => $this->customer,
+            'body' => $this->review,
+            'rating' => $this->star == 0 ? 'No rating submitted' : $this->star
+        ];
+    }
+}
